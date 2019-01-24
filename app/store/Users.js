430feaaf20176580fbe1207fd1b935909b82fe3d
pkindalov <panayotkindalov@gmail.com>
2019-01-25 00:29:26 +0200

    Created model User in app/model directory and store for users - Users.js in app/store.

---
+++ app/store/Users.js
@@ -0,0 +1,13 @@
+Ext.define('AM.store.Users', {
+    extend: 'Ext.data.Store',
+    model: 'AM.model.User',
+    data: [{
+            name: 'Ed',
+            email: 'ed@sencha.com'
+        },
+        {
+            name: 'Tommy',
+            email: 'tommy@sencha.com'
+        }
+    ]
+});
