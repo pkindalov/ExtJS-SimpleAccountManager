872252167f1e4c7552f304dc295dd40873f00daf
pkindalov <panayotkindalov@gmail.com>
2019-01-25 13:41:06 +0200

    Created users.json to save information about users.  Updated Users store to use proxy and to get users info from the users.json file in data directory

--- app/store/Users.js
+++ app/store/Users.js
@@ -1,13 +1,15 @@
 Ext.define('AM.store.Users', {
     extend: 'Ext.data.Store',
     model: 'AM.model.User',
-    data: [{
-            name: 'Ed',
-            email: 'ed@sencha.com'
-        },
-        {
-            name: 'Tommy',
-            email: 'tommy@sencha.com'
+    autoLoad: true,
+
+    proxy: {
+        type: 'ajax',
+        url: 'data/users.json',
+        reader: {
+            type: 'json',
+            root: 'users',
+            successProperty: 'success'
         }
-    ]
+    }
 });
